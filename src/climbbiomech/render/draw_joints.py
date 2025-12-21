51a5ac61258f13e5afea485289308523008ea990
Wilson Feng <wilsonxfeng@gmail.com>
2025-12-21 18:49:17 -0500

    Guarded out of bounds

--- src/climbbiomech/render/draw_joints.py
+++ src/climbbiomech/render/draw_joints.py
@@ -6,17 +6,21 @@ from climbbiomech.pose.landmark_enumeration import PoseLandmark
 
 
 def draw_joints(frame, result):
+
+	if not result.pose_landmarks:
+		return frame
+
 	pose = result.pose_landmarks[0]
 	H, W = frame.shape[0], frame.shape[1]
 
 	for joint in range(len(pose)):
 
 		joint_info = pose[joint]
+		x_px = int(joint_info.x * W)
+		y_px = int(joint_info.y * H)
 
-		if joint_info.visibility >= 0.4 and joint_info.presence >= 0.4:
+		if joint_info.visibility >= 0.4 and joint_info.presence >= 0.4 and x_px < W and y_px < H:
 
-			x_px = int(joint_info.x * W)
-			y_px = int(joint_info.y * H)
 			coords = [x_px, y_px]
 			color = [255, 255, 255]
 
