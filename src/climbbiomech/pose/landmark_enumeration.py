9aa378fd92e4d856f3396495cf9a4d39f3c04473
Wilson Feng <wilsonxfeng@gmail.com>
2025-12-21 20:27:56 -0500

    Bone Tracking

--- src/climbbiomech/pose/landmark_enumeration.py
+++ src/climbbiomech/pose/landmark_enumeration.py
@@ -1,39 +1,72 @@
 from enum import IntEnum
 
-class PoseLandmark(IntEnum):
-    NOSE = 0
-    LEFT_EYE_INNER = 1
-    LEFT_EYE = 2
-    LEFT_EYE_OUTER = 3
-    RIGHT_EYE_INNER = 4
-    RIGHT_EYE = 5
-    RIGHT_EYE_OUTER = 6
-    LEFT_EAR = 7
-    RIGHT_EAR = 8
-    MOUTH_LEFT = 9
-    MOUTH_RIGHT = 10
+BoneLandmarks = [
+    ["LEFT_SHOULDER", "RIGHT_SHOULDER"],
+    ["LEFT_HIP", "RIGHT_HIP"],
+    ["LEFT_SHOULDER", "LEFT_HIP"],
+    ["RIGHT_SHOULDER", "RIGHT_HIP"],
+    ["RIGHT_SHOULDER", "RIGHT_HIP"],
+    ["LEFT_SHOULDER", "LEFT_ELBOW"],
+    ["LEFT_ELBOW", "LEFT_WRIST"],
+    ["RIGHT_SHOULDER", "RIGHT_ELBOW"],
+    ["RIGHT_ELBOW", "RIGHT_WRIST"],
+    ["LEFT_HIP", "LEFT_KNEE"],
+    ["RIGHT_HIP", "RIGHT_KNEE"],
+    ["LEFT_KNEE", "LEFT_ANKLE"],
+    ["RIGHT_KNEE", "RIGHT_ANKLE"]]
 
-    LEFT_SHOULDER = 11
-    RIGHT_SHOULDER = 12
-    LEFT_ELBOW = 13
-    RIGHT_ELBOW = 14
-    LEFT_WRIST = 15
-    RIGHT_WRIST = 16
+ImportantLandmarks = [
+    "LEFT_SHOULDER",
+    "RIGHT_SHOULDER",
+    "LEFT_ELBOW",
+    "RIGHT_ELBOW",
+    "LEFT_WRIST",
+    "RIGHT_WRIST",
+    "LEFT_HIP",
+    "RIGHT_HIP",
+    "LEFT_KNEE",
+    "RIGHT_KNEE",
+    "LEFT_ANKLE",
+    "RIGHT_ANKLE",
+    "LEFT_FOOT_INDEX",
+    "RIGHT_FOOT_INDEX",
+    "LEFT_HEEL",
+    "RIGHT_HEEL"]
 
-    LEFT_PINKY = 17
-    RIGHT_PINKY = 18
-    LEFT_INDEX = 19
-    RIGHT_INDEX = 20
-    LEFT_THUMB = 21
-    RIGHT_THUMB = 22
+PoseLandmarks = {
+    "NOSE": 0,
+    "LEFT_EYE_INNER": 1,
+    "LEFT_EYE": 2,
+    "LEFT_EYE_OUTER": 3,
+    "RIGHT_EYE_INNER": 4,
+    "RIGHT_EYE": 5,
+    "RIGHT_EYE_OUTER": 6,
+    "LEFT_EAR": 7,
+    "RIGHT_EAR": 8,
+    "MOUTH_LEFT": 9,
+    "MOUTH_RIGHT": 10,
 
-    LEFT_HIP = 23
-    RIGHT_HIP = 24
-    LEFT_KNEE = 25
-    RIGHT_KNEE = 26
-    LEFT_ANKLE = 27
-    RIGHT_ANKLE = 28
-    LEFT_HEEL = 29
-    RIGHT_HEEL = 30
-    LEFT_FOOT_INDEX = 31
-    RIGHT_FOOT_INDEX = 32
+    "LEFT_SHOULDER": 11,
+    "RIGHT_SHOULDER": 12,
+    "LEFT_ELBOW": 13,
+    "RIGHT_ELBOW": 14,
+    "LEFT_WRIST": 15,
+    "RIGHT_WRIST": 16,
+
+    "LEFT_PINKY": 17,
+    "RIGHT_PINKY": 18,
+    "LEFT_INDEX": 19,
+    "RIGHT_INDEX": 20,
+    "LEFT_THUMB": 21,
+    "RIGHT_THUMB": 22,
+
+    "LEFT_HIP": 23,
+    "RIGHT_HIP": 24,
+    "LEFT_KNEE": 25,
+    "RIGHT_KNEE": 26,
+    "LEFT_ANKLE": 27,
+    "RIGHT_ANKLE": 28,
+    "LEFT_HEEL": 29,
+    "RIGHT_HEEL": 30,
+    "LEFT_FOOT_INDEX": 31,
+    "RIGHT_FOOT_INDEX": 32}
--- src/climbbiomech/render/draw_joints.py
+++ src/climbbiomech/render/draw_joints.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import cv2
 import typer
 import numpy
-from climbbiomech.pose.landmark_enumeration import PoseLandmark
+from climbbiomech.pose.landmark_enumeration import PoseLandmarks, ImportantLandmarks, BoneLandmarks
 
 
 def draw_joints(frame, result):
@@ -12,19 +12,35 @@ def draw_joints(frame, result):
 
 	pose = result.pose_landmarks[0]
 	H, W = frame.shape[0], frame.shape[1]
+	color = [255, 255, 255]
 
-	for joint in range(len(pose)):
+	for joint in ImportantLandmarks:
 
-		joint_info = pose[joint]
+		joint_info = pose[PoseLandmarks[joint]]
 		x_px = int(joint_info.x * W)
 		y_px = int(joint_info.y * H)
 
 		if joint_info.visibility >= 0.4 and joint_info.presence >= 0.4 and x_px < W and y_px < H:
 
 			coords = [x_px, y_px]
-			color = [255, 255, 255]
 
-			cv2.circle(frame, coords, 10, color, 1)
+			cv2.circle(frame, coords, 10, color, 5)
+
+	for bone in BoneLandmarks:
+		start = pose[PoseLandmarks[bone[0]]]
+		end = pose[PoseLandmarks[bone[1]]]
+
+		x1_px = int(start.x * W)
+		x2_px = int(end.x * W)
+		y1_px = int(start.y * H)
+		y2_px = int(end.y * H)
+
+		startcoords = [x1_px, y1_px]
+		endcoords = [x2_px, y2_px]
+
+		cv2.line(frame, startcoords, endcoords, color, 3)
+
+
 
 	return frame
 
